09456c3947161105ccf229de81e0460e87f287f8
Terje Torkelsen <terje.torkelsen@avinor.no>
2019-11-12 16:18:39 +0100

    Updating example to match new setup

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -4,7 +4,7 @@ module "spoke" {
     name = "spoke"
     resource_group_name = "networking-spoke-rg"
     location = "westeurope"
-    address_space = "10.0.0.0/16"
+    address_space = ["10.0.0.0/16"]
     firewall_ip = "10.0.0.4"
     hub_virtual_network_id = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/mygroup1/providers/Microsoft.Network/virtualNetworks/myvnet1"
 
@@ -14,6 +14,21 @@ module "spoke" {
             address_prefix = "10.0.0.0/24"
             service_endpoints = []
             security_rules = []
+            disable_firewall_route = false
+        },
+        {
+            name = "subnet_null"
+            address_prefix = "10.0.1.0/24"
+            service_endpoints = []
+            security_rules = []
+            disable_firewall_route = null
+        },
+        {
+            name = "nofirewall"
+            address_prefix = "10.0.2.0/24"
+            service_endpoints = []
+            security_rules = []
+            disable_firewall_route = true
         },
     ]
 }
