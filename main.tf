1f359675ad92861501c11a53fd3a3aab2ee3ec2b
Øyvind Strømmen <insula.ventus@gmail.com>
2021-04-21 13:01:17 +0200

    Require terraform >= v0.12.26

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 0.12.6"
+  required_version = ">= 0.12.26"
   required_providers {
     azurerm = { version = "~> 2.53.0" }
     null    = { version = "~> 2.1" }
