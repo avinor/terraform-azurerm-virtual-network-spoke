02da041575e790ea15e9d1c4bd844daa6ae825fc
Pavlo Prykhodko <pavlo.prykhodko@avinor.no>
2021-06-11 13:47:17 +0200

    Add each available diagnostics category, but enable only those we want

--- main.tf
+++ main.tf
@@ -139,33 +139,47 @@ resource "azurerm_virtual_network" "vnet" {
   tags = var.tags
 }
 
+data "azurerm_monitor_diagnostic_categories" "default" {
+  resource_id = azurerm_virtual_network.vnet.id
+}
+
 resource "azurerm_monitor_diagnostic_setting" "vnet" {
   count                          = var.diagnostics != null ? 1 : 0
-  name                           = "vnet-diag"
+  name                           = "${var.name}-vnet-diag"
   target_resource_id             = azurerm_virtual_network.vnet.id
   log_analytics_workspace_id     = local.parsed_diag.log_analytics_id
   eventhub_authorization_rule_id = local.parsed_diag.event_hub_auth_id
   eventhub_name                  = local.parsed_diag.event_hub_auth_id != null ? var.diagnostics.eventhub_name : null
   storage_account_id             = local.parsed_diag.storage_account_id
 
+  # For each available log category, check if it should be enabled and set enabled = true if it should.
+  # All other categories are created with enabled = false to prevent TF from showing changes happening with each plan/apply.
+  # Ref: https://github.com/terraform-providers/terraform-provider-azurerm/issues/7235
   dynamic "log" {
-    for_each = setintersection(local.parsed_diag.log, local.diag_vnet_logs)
+    for_each = data.azurerm_monitor_diagnostic_categories.default.logs
     content {
       category = log.value
+      enabled  = contains(local.parsed_diag.log, "all") || contains(local.parsed_diag.log, log.value)
 
       retention_policy {
         enabled = false
+        days    = 0
       }
     }
   }
 
+  # For each available metric category, check if it should be enabled and set enabled = true if it should.
+  # All other categories are created with enabled = false to prevent TF from showing changes happening with each plan/apply.
+  # Ref: https://github.com/terraform-providers/terraform-provider-azurerm/issues/7235
   dynamic "metric" {
-    for_each = setintersection(local.parsed_diag.metric, local.diag_vnet_metrics)
+    for_each = data.azurerm_monitor_diagnostic_categories.default.metrics
     content {
       category = metric.value
+      enabled  = contains(local.parsed_diag.metric, "all") || contains(local.parsed_diag.metric, metric.value)
 
       retention_policy {
         enabled = false
+        days    = 0
       }
     }
   }
