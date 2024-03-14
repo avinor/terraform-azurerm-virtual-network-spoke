77d6f5fcefe51542ec8b02c22ab48d9b3487167b
Emil Kordahl <89079229+emilkor1@users.noreply.github.com>
2024-03-14 09:12:17 +0100

    Upgrade providers and remove deprecations

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.69.0"
+      version = "~> 3.95.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.4.3"
+      version = "~> 3.6.0"
     }
   }
 }
@@ -134,18 +134,16 @@ resource "azurerm_monitor_diagnostic_setting" "vnet" {
   # For each available log category, check if it should be enabled and set enabled = true if it should.
   # All other categories are created with enabled = false to prevent TF from showing changes happening with each plan/apply.
   # Ref: https://github.com/terraform-providers/terraform-provider-azurerm/issues/7235
-  dynamic "log" {
-    for_each = data.azurerm_monitor_diagnostic_categories.vnet.log_category_types
+  dynamic "enabled_log" {
+    for_each = {
+      for k, v in data.azurerm_monitor_diagnostic_categories.vnet.log_category_types : k => v
+      if contains(local.parsed_diag.log, "all") || contains(local.parsed_diag.log, v)
+    }
     content {
-      category = log.value
-      enabled  = contains(local.parsed_diag.log, "all") || contains(local.parsed_diag.log, log.value)
-
-      retention_policy {
-        enabled = false
-        days    = 0
-      }
+      category = enabled_log.value
     }
   }
+
 
   # For each available metric category, check if it should be enabled and set enabled = true if it should.
   # All other categories are created with enabled = false to prevent TF from showing changes happening with each plan/apply.
@@ -155,11 +153,6 @@ resource "azurerm_monitor_diagnostic_setting" "vnet" {
     content {
       category = metric.value
       enabled  = contains(local.parsed_diag.metric, "all") || contains(local.parsed_diag.metric, metric.value)
-
-      retention_policy {
-        enabled = false
-        days    = 0
-      }
     }
   }
 }
@@ -196,7 +189,7 @@ resource "azurerm_subnet" "vnet" {
 
 module "storage" {
   source  = "avinor/storage-account/azurerm"
-  version = "3.5.3"
+  version = "3.5.5"
 
   name                  = var.name
   resource_group_name   = azurerm_resource_group.vnet.name
@@ -324,16 +317,13 @@ resource "azurerm_monitor_diagnostic_setting" "nsg" {
   eventhub_name                  = local.parsed_diag.event_hub_auth_id != null ? var.diagnostics.eventhub_name : null
   storage_account_id             = local.parsed_diag.storage_account_id
 
-  dynamic "log" {
-    for_each = data.azurerm_monitor_diagnostic_categories.nsg[each.key].logs
+  dynamic "enabled_log" {
+    for_each = {
+      for k, v in data.azurerm_monitor_diagnostic_categories.nsg[each.key].logs : k => v
+      if contains(local.parsed_diag.log, "all") || contains(local.parsed_diag.log, v)
+    }
     content {
-      category = log.value
-      enabled  = contains(local.parsed_diag.log, "all") || contains(local.parsed_diag.log, log.value)
-
-      retention_policy {
-        enabled = false
-        days    = 0
-      }
+      category = enabled_log.value
     }
   }
 
@@ -345,11 +335,6 @@ resource "azurerm_monitor_diagnostic_setting" "nsg" {
     content {
       category = metric.value
       enabled  = contains(local.parsed_diag.metric, "all") || contains(local.parsed_diag.metric, metric.value)
-
-      retention_policy {
-        enabled = false
-        days    = 0
-      }
     }
   }
 }
